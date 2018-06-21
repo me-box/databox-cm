fa25426a6a5bc770f2b655939a75abdac264f587
ToshBrown <Toshbrown@gmail.com>
2018-06-21 16:29:18 +0100

    Add web-socket proxy
    Needs testing

--- databoxProxyMiddleware.go
+++ databoxProxyMiddleware.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 	"sync"
 
+	"github.com/gorilla/websocket"
 	libDatabox "github.com/toshbrown/lib-go-databox"
 )
 
@@ -36,24 +38,128 @@ func (d *DataboxProxyMiddleware) ProxyMiddleware(next http.Handler) http.Handler
 
 	proxy := func(w http.ResponseWriter, r *http.Request) {
 
-		parts := strings.Split(r.URL.Path, "/")
-
-		//d.mutex.Lock()
-		//defer d.mutex.Unlock()
-		if _, ok := d.proxyList[parts[1]]; ok == false {
-			//no need to proxy
-			next.ServeHTTP(w, r)
-			return
+		if websocket.IsWebSocketUpgrade(r) {
+			d.proxyWebSocket(w, r, next)
+		} else {
+			d.proxyHTTP(w, r, next)
 		}
 
-		RequestURI := "https://" + parts[1] + ":8080/" + strings.Join(parts[2:], "/")
+	}
 
-		if r.URL.RawQuery != "" {
-			RequestURI = RequestURI + "?" + r.URL.RawQuery
+	return http.HandlerFunc(proxy)
+}
+
+func (d *DataboxProxyMiddleware) proxyWebSocket(w http.ResponseWriter, r *http.Request, next http.Handler) {
+
+	parts := strings.Split(r.URL.Path, "/")
+
+	if _, ok := d.proxyList[parts[1]]; ok == false {
+		//no need to proxy
+		next.ServeHTTP(w, r)
+		return
+	}
+
+	backendURL := "wss://" + parts[1] + ":8080/" + strings.Join(parts[2:], "/")
+
+	dialer := &websocket.Dialer{}
+
+	// Pass headers from the incoming request to the dialer to forward them to
+	// the final destinations.
+	requestHeader := http.Header{}
+	if origin := r.Header.Get("Origin"); origin != "" {
+		requestHeader.Add("Origin", origin)
+	}
+	for _, prot := range r.Header[http.CanonicalHeaderKey("Sec-WebSocket-Protocol")] {
+		requestHeader.Add("Sec-WebSocket-Protocol", prot)
+	}
+	for _, cookie := range r.Header[http.CanonicalHeaderKey("Cookie")] {
+		requestHeader.Add("Cookie", cookie)
+	}
+
+	connBackend, resp, err := dialer.Dial(backendURL, requestHeader)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection to backend", http.StatusBadRequest)
+		return
+	}
+	defer connBackend.Close()
+
+	upgradeHeader := http.Header{}
+	if hdr := resp.Header.Get("Sec-Websocket-Protocol"); hdr != "" {
+		upgradeHeader.Set("Sec-Websocket-Protocol", hdr)
+	}
+	if hdr := resp.Header.Get("Set-Cookie"); hdr != "" {
+		upgradeHeader.Set("Set-Cookie", hdr)
+	}
+
+	conn, err := websocket.Upgrade(w, r, upgradeHeader, 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+	}
+	defer connBackend.Close()
+
+	errClient := make(chan error, 1)
+	errBackend := make(chan error, 1)
+	copy := func(from *websocket.Conn, to *websocket.Conn, errorChan chan error) {
+		for {
+			msgType, msg, err := from.ReadMessage()
+			if err != nil {
+				m := websocket.FormatCloseMessage(websocket.CloseNormalClosure, fmt.Sprintf("%v", err))
+				if e, ok := err.(*websocket.CloseError); ok {
+					if e.Code != websocket.CloseNoStatusReceived {
+						m = websocket.FormatCloseMessage(e.Code, e.Text)
+					}
+				}
+				errorChan <- err
+				to.WriteMessage(websocket.CloseMessage, m)
+				break
+			}
+			err = to.WriteMessage(msgType, msg)
+			if err != nil {
+				errorChan <- err
+				break
+			}
 		}
+	}
 
-		libDatabox.Debug("Proxying internal request to  " + RequestURI)
+	go copy(conn, connBackend, errClient)
+	go copy(connBackend, conn, errBackend)
 
+	var message string
+	select {
+	case err = <-errClient:
+		message = "Error when copying from backend to client: %v"
+	case err = <-errBackend:
+		message = "Error when copying from client to backend: %v"
+
+	}
+	if e, ok := err.(*websocket.CloseError); !ok || e.Code == websocket.CloseAbnormalClosure {
+		libDatabox.Err(message + " " + err.Error())
+	}
+
+	return
+}
+
+func (d *DataboxProxyMiddleware) proxyHTTP(w http.ResponseWriter, r *http.Request, next http.Handler) {
+	parts := strings.Split(r.URL.Path, "/")
+
+	if _, ok := d.proxyList[parts[1]]; ok == false {
+		//no need to proxy
+		next.ServeHTTP(w, r)
+		return
+	}
+
+	RequestURI := "https://" + parts[1] + ":8080/" + strings.Join(parts[2:], "/")
+
+	if r.URL.RawQuery != "" {
+		RequestURI = RequestURI + "?" + r.URL.RawQuery
+	}
+
+	libDatabox.Debug("Proxying internal request to  " + RequestURI)
+
+	var wg sync.WaitGroup
+
+	copy := func() {
+		defer wg.Done()
 		req, err := http.NewRequest(r.Method, RequestURI, r.Body)
 		for name, value := range r.Header {
 			req.Header.Set(name, value[0])
@@ -72,10 +178,14 @@ func (d *DataboxProxyMiddleware) ProxyMiddleware(next http.Handler) http.Handler
 		}
 		w.WriteHeader(resp.StatusCode)
 		io.Copy(w, resp.Body)
-		return
+
 	}
 
-	return http.HandlerFunc(proxy)
+	wg.Add(1)
+	go copy()
+	wg.Wait()
+
+	return
 }
 
 func (d *DataboxProxyMiddleware) Add(containerName string) {
